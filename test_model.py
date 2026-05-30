a03f0186802580429b96d7e6f45b6932543a3240
Mukadder Gur <mukaddergur085@gmail.com>
2026-05-30 13:25:57 +0300

    düzenlendi

--- test_model.py
+++ test_model.py
@@ -1,10 +1,6 @@
 import joblib
 import os
-
-# model.pkl dosyasının bulunduğu yolu yaz
 model_path = os.path.join('ml', 'model.pkl') 
 model = joblib.load(model_path)
-
 print(f"Model Tipi: {type(model)}")
-# Eğer model içinde predict_proba varsa, bu genellikle bir Classifier'dır
 print(f"Sınıflandırıcı mı?: {hasattr(model, 'predict_proba')}")
